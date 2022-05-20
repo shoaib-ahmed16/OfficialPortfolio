c59bf410aa1e27959600f6bf57843f7528abcd27
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-20 12:09:04 +0530

    ASda

--- script/Email.js
+++ script/Email.js
@@ -3,18 +3,18 @@
     e.preventDefault()
     try {
       let data = {
-      'name': `${document.getElementById('form_name').value}`,
-      'email': `${document.getElementById('form_email').value}`,
-      'subject':`${document.getElementById('subject_mail').value}`,
-      'text': `${document.getElementById('message_text').value}`
+      name:document.getElementById('form_name').value,
+      email:document.getElementById('form_email').value,
+      subject:document.getElementById('subject_mail').value,
+      text: document.getElementById('message_text').value
     }
       console.log(data)
-      let res = await fetch('https://shoaibsmtp.herokuapp.com/email', {
-        mode: 'no-cors',
-        method: 'POST',
+      let res = await fetch("https://shoaibsmtp.herokuapp.com/email", {
+        mode: "no-cors",
+        method: "POST",
         body: data,
         headers: {
-          'content-Type': 'application/json'
+          "content-Type": "application/json",
         }
       })
       let data1 = await res.json()
