38a3e568efe47817ebbb3e801245593d74f93173
Shoaib Ahmed <96101435+shoaib-ahmed16@users.noreply.github.com>
2022-05-21 14:24:28 +0530

    asdsd

--- script/Email.js
+++ script/Email.js
@@ -3,24 +3,33 @@
     e.preventDefault()
     try {
       let data = {
-      name:document.querySelector('#form_name').value,
-      email:document.querySelector('#form_email').value,
-      subject:document.querySelector('#subject_mail').value,
-      text: document.querySelector('#message_text').value
+        name:document.querySelector('#form_name').value,
+        email:document.querySelector('#form_email').value,
+        subject:document.querySelector('#subject_mail').value,
+        text: document.querySelector('#message_text').value
     }
       console.log(data)
       let datajson =JSON.stringify(data);
       console.log(datajson)
-      let res = await fetch("https://shoaibsmtp.herokuapp.com/email",{
-        mode:"no-cors",
+      // $.ajax({
+      //   method: "POST",
+      //   url: "https://shoaibsmtp.herokuapp.com/email",
+      //   params: data,
+      //   success: function(result){
+      //     console.log(result);
+      //   }
+      // })
+      let res = await fetch("http://localhost:3000/email",{
         method:"POST",
         body:datajson,
         headers:{
           "Content-Type": "application/json",
         }
+      }).then((res) => {
+        console.log(res.status);
       })
-      let data1 = await res.json()
-      console.log({ data: data1 })
+      // let data1 = await res.json()
+      // console.log({ data: data1 })
     } catch (error) {
       console.log({ err: error.message })
     }
